d336d5bff97212db81e963d5f395596cb9d34cc6
Dai Nguyen <dainguyen@gmail.com>
2016-09-07 12:06:32 -0700

    initial commit

---
+++ server/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/css',express.static(path.join(__dirname, '../client/css')));
+app.use('/js',express.static(path.join(__dirname, '../client/js')));
+app.use('/templates',express.static(path.join(__dirname, '../client/js/templates')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
+
+module.exports = app;
