d04cc120b01ae3f2d15d77af95e70d1a38d5fdfc
Dai Nguyen <dainguyen@gmail.com>
2016-09-25 22:47:11 -0700

    start adding video detection

--- client/dist/app.js
+++ client/dist/app.js
@@ -84,6 +84,20 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
   function MainController(heatMapService) {
     var droneDiv = document.getElementById("drone-stream");
     new NodecopterStream(droneDiv);
+
+    var colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
+    colors.on('track', function (event) {
+      if (event.data.length === 0) {
+        console.log('no colors detected');
+      } else {
+        event.data.forEach(function (rect) {
+          console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
+        });
+      }
+    });
+
+    var canvas = document.querySelector('#drone-stream canvas');
+    tracking.track(canvas, colors);
   }
 })();
 
--- client/src/js/controllers/main.controller.js
+++ client/src/js/controllers/main.controller.js
@@ -8,5 +8,19 @@
   function MainController(heatMapService) {
     let droneDiv = document.getElementById("drone-stream");
     new NodecopterStream(droneDiv);
+
+    let colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
+    colors.on('track', function(event) {
+      if (event.data.length === 0) {
+        console.log('no colors detected');
+      } else {
+        event.data.forEach(function(rect) {
+          console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
+        });
+      }
+    });
+
+    let canvas = document.querySelector('#drone-stream canvas');
+    tracking.track(canvas, colors);
   }
 })();
